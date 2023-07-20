351467bc38702daf0e28995ac32822459ba432d4
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-07-20 19:18:58 +0100

    adding the sidebar component to include categories in the sidebar menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
+const selectedCategory = 'New';
 
 const Sidebar = () => (
     <Stack
@@ -11,6 +12,22 @@ const Sidebar = () => (
         flexDirection: { md: 'column'},
     }}
     >
+      {categories.map((category) => (
+        <button
+        className='category-btn'
+        style={{
+            background: category.name === selectedCategory && '#FC1503',
+            color: 'white'
+        }}
+    
+        >
+        <span>
+            {category.icon}
+        </span>
+        <span>{category.name}</span>
+
+        </button>
+      ))}
 
     </Stack>
   )
