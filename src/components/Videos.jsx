07e25656e837101f776a07f56e522cf3830cc511
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-07-21 07:23:08 +0100

    confirming video card results displayed on the feed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,7 @@
 import { Stack, Box } from '@mui/material';
 
+import { VideoCard, ChannelCard } from './';
+
 //destructure props and get videos
 const Videos = ({ videos }) => {
   return (
@@ -8,7 +10,7 @@ const Videos = ({ videos }) => {
        {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
+          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
         </Box>
        ))}
     </Stack>
