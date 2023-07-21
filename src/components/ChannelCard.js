4ef576ec90a2bbc95c53c58f00e728af4866a06d
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-07-21 21:30:14 +0100

    video and channel card styling improved

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -5,7 +5,11 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 const ChannelCard = ({ channelDetail }) => (
-    <Box sx={{ boxShadow: 'none', borderRadius: '20px', }}>
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px',
+       display: 'flex', justifyContent: 'center',
+       alignItems: 'center', width: { xs: '365px', md: '320px'},
+       height: '326px', margin: 'auto'
+      }}>
        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
            <CardContent sx={{ display: 'flex', flexDirection:
            'column', justifyContent: 'center', textAlign: 'center',
@@ -17,6 +21,16 @@ const ChannelCard = ({ channelDetail }) => (
                sx={{ borderRadius: '50%', height: '180px',
                width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
               />
+              <Typography variant="h6">
+                {channelDetail?.snippet?.title}
+                <CheckCircle sx={{ fontSize: 14, color: 'gray',
+                ml: '5px' }}/>
+              </Typography>
+               {channelDetail?.statistics?.subscriberCount && (
+                <Typography>
+                  {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()}
+                </Typography>
+               )}
            </CardContent>
        </Link>
     </Box>
