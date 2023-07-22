7139f4925346fbc40038392603b856720056e5ed
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-07-22 21:55:00 +0100

    improving the styling of the videoDetail component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,9 @@ import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
 //destructure props and get videos
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   return (
-    <Stack direction="row" flexWrap="wrap" //layout the info we want to display
+    <Stack direction= {direction || "row"} flexWrap="wrap" //layout the info we want to display
      justifyContent="start" gap={2}>
        {videos.map((item, idx) => (
         <Box key={idx}>
